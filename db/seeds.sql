58a6a2e306a0e836825d8fdaabe982adb9ae2f6c
rivendell <webwyler@gmail.com>
2019-09-18 21:12:53 -0500

    created burgers db and burgers table, added burger names in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Inserted a set of records into the table
+INSERT INTO burgers (burger_name)
+VALUES ("Impossible Double Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Pizza with Onions Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Fries Egg with Cheese Burger");
